38f76038b669223c5c3e3dbd5624d6ee3febe52f
AmmarAlyy10 <ammar.aly@maxab.io>
2022-08-17 16:53:53 +0200

    hello java class remote in local 2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@ package git_ex_1.src;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("hello java");
-        System.out.println("hello java in git remote repo online");
+        System.out.println("hello java in git remote repo");
+        System.out.println("hello java in git remote repo in local 2");
     }
 }
