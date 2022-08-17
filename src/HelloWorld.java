1c7d17e73ce82cd0caca4ed633dc28b552930e4c
AmmarAlyy10 <108274849+AmmarAlyy10@users.noreply.github.com>
2022-08-17 16:46:29 +0200

    Update HelloWorld.java
    commit in remote repo

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ package git_ex_1.src;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("hello java in git");
+        System.out.println("hello java");
+        System.out.println("hello java in git remote repo");
     }
 }
